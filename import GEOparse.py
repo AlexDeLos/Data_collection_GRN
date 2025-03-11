655116670bec7a679f761c8f887f7d8daae47c76
Alex <64327268+AlexDeLos@users.noreply.github.com>
2025-03-11 15:29:04 +0100

    initial commit

---
+++ import GEOparse.py
@@ -0,0 +1,93 @@
+import GEOparse
+import pandas as pd
+
+def get_geo_list(path:str):
+    read =  pd.read_csv(path)
+    read = read.loc[read['depository_source'] == "GEO"]
+    read = read.loc[read['species'] == "Arabidopsis thaliana"]
+    return list(read["depository_accession"])
+def mapping(x):
+    if type(x) is str:
+        return x.upper()
+    else:
+        return x
+    
+csv_list= "data_addresses.csv"
+geo_list = get_geo_list(csv_list)
+df = pd.DataFrame()
+first =True
+duplicate_count = {}
+for geo in geo_list:
+    try:
+        gse = GEOparse.get_GEO(geo=geo, destdir="./data",silent=True)
+        print(df)
+        print(gse)
+
+        key = list(gse.gpls)
+        key = key[0]
+        gpl_table=gse.gpls[key].table
+        try:
+            probe_to_gene_map = dict(zip(gpl_table["ID"], gpl_table["ORF"].map(mapping)))
+        except Exception as error:
+            print("---- ERROR MAKING MAP ----")
+            print(error)
+            continue
+        for gsm_name, gsm in gse.gsms.items():
+            print("Name: ", gsm_name)
+            print ("Table data:",)
+            print (gsm.table.head())
+            in_df = gsm.table
+            in_df['ID_REF'] = in_df['ID_REF'].map(probe_to_gene_map)
+            # How to drop the numbers
+            in_df = in_df.dropna()
+
+            in_df.rename(columns={'VALUE': gsm_name}, inplace=True)
+            in_df.set_index('ID_REF',inplace = True)
+            # we take only the arabidopsis thaliana genges
+            in_df.drop(in_df[~in_df.index.str.match(r'^At[1-5MC]g\d{5}$', case=False)].index, inplace=True) # we take only the a
+            in_df = in_df.filter([gsm_name])
+
+            if df.index.duplicated().any():
+                print("Duplicates in df:", df[df.index.duplicated(keep=False)])
+
+                # Create a dictionary to keep track of the count of duplicates
+                duplicate_count = df.index.value_counts().to_dict()
+
+                # Group by the index column and calculate the mean of the values
+                df = df.groupby('ID_REF')[in_df.columns[0]].mean().reset_index() # assuming there 
+
+            if in_df.index.duplicated().any():
+                print("Duplicates in df:", in_df[in_df.index.duplicated(keep=False)])
+
+                # Create a dictionary to keep track of the count of duplicates
+                duplicate_count = in_df.index.value_counts().to_dict()
+
+                # Group by the index column and calculate the mean of the values
+                in_df = in_df.groupby('ID_REF')[in_df.columns[0]].mean().reset_index() # assuming there is only one value
+
+            try:
+                if in_df.index.name != 'ID_REF':
+                    in_df.set_index('ID_REF',inplace = True)
+                df = pd.concat([df, in_df], axis=1)
+
+                # df = df.merge(, how='outer', left_index=True, right_index=True)
+            except Exception as error:
+                print(error)
+                pass
+            
+    except Exception as error:
+        print(error)
+        print("-----An error occured, probably an empty dataframe")
+
+
+#! For now we ignore GPLs
+# print()
+# print("GPL example:")
+# for gpl_name, gpl in gse.gpls.items():
+#     print("Name: ", gpl_name)
+#     print("Metadata:",)
+#     for key, value in gpl.metadata.items():
+#         print(" - %s : %s" % (key, ", ".join(value)))
+#     print("Table data:",)
+#     print(gpl.table.head())
+#     break
