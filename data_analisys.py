758b826790c87befd533ff91b72b2924c2587d24
Alex <64327268+AlexDeLos@users.noreply.github.com>
2025-03-26 06:39:18 -0800

    Update data_analisys.py

--- data_analisys.py
+++ data_analisys.py
@@ -11,12 +11,12 @@ plot_nan = False
 
 # Get CSV files list from a folder
 path = '/tudelft.net/staff-umbrella/AT GE Datasets/df_nan'
-out_path = "/tudelft.net/staff-umbrella/AT GE Datasets/figures"
+out_path = '/tudelft.net/staff-umbrella/AT GE Datasets/figures'
 try:
     filtered_df = pd.read_csv(path+'/filter.csv', index_col=0)
-    print("succesfully loaded data")
+    print('succesfully loaded data')
 except:
-    csv_files = glob.glob(path + "/*.csv")
+    csv_files = glob.glob(path + '/*.csv')
 
     # Read each CSV file into DataFrame
     # This creates a list of dataframes
@@ -32,36 +32,38 @@ except:
 
     big_df.to_csv(path+'/complete.csv')
 
-
     # filter the data on 20%
-    # Calculate the percentage of NaN values in each column
-    nan_percentage = big_df.isna().mean() * 100
-
-    # Filter columns where NaN percentage <= 20%
-    filtered_columns = nan_percentage[nan_percentage <= 20].index
-
-    # Keep only the filtered columns
-    filtered_df = big_df[filtered_columns]
-
-
-    nan_percentage_rows = filtered_df.isna().mean(axis=1) * 100
+    nan_percentage_genes = big_df.isna().mean(axis=1) * 100
 
     # Filter rows where NaN percentage <= 20%
-    filtered_rows = nan_percentage_rows[nan_percentage_rows <= 20].index
+    filtered_genes = nan_percentage_genes[nan_percentage_genes <= 20].index
 
     # Keep only the filtered rows
-    filtered_df = filtered_df.loc[filtered_rows]
+    filtered_df = big_df.loc[filtered_genes]
+
+
+    # Calculate the percentage of NaN values in each column
+    nan_percentage_samples = filtered_df.isna().mean() * 100
+
+    # Filter columns where NaN percentage <= 20%
+    filtered_columns = nan_percentage_samples[nan_percentage_samples <= 20].index
+
+    # Keep only the filtered columns
+    filtered_df = filtered_genes[filtered_columns]
+
+
+
 
 
     filtered_df.to_csv(path+'/filter.csv')
 
-print("data loaded")
+print('data loaded')
 big_df = filtered_df
 
 matrix = big_df.to_numpy()
 matrix_nan = big_df.isna().to_numpy()
 
-chromosomes = ["1",'2','3','4','5']
+chromosomes = ['1','2','3','4','5']
 indices:list[int] = get_first_indexs(big_df.index,chromosomes)
 
 if plot_nan:
@@ -81,39 +83,39 @@ if plot_nan:
             max = len(matrix)
         plt.bar(range(len(row_nan_count.index[min:max])),row_nan_count.values[min:max])
         plt.ylim(0,1850)
-        plt.xlabel("Genes")
-        plt.ylabel("Missing number of data points")
+        plt.xlabel('Genes')
+        plt.ylabel('Missing number of data points')
         plt.savefig(out_path+'/row/0.row_dis'+chromosomes[i]+'.svg')
         plt.close()
 
     plt.bar(range(len(col_nan_count.index)),col_nan_count.values)
-    plt.xlabel("Experiments")
-    plt.ylabel("Missing number of data points")
+    plt.xlabel('Experiments')
+    plt.ylabel('Missing number of data points')
     plt.savefig(out_path+'/col/0.col_dis.svg')
     plt.close()
 
-print("plotted Nans", plot_nan)
+print('plotted Nans', plot_nan)
 np.nan_to_num(matrix,copy=False)
-print("nans filled with 0")
+print('nans filled with 0')
 
 #! Normalize data
 matrix = normalize_2d(matrix)
-print("Normalized matrix")
+print('Normalized matrix')
 plot_sim_matrix(matrix,indices,chromosomes, save_loc=out_path)
-print("plotting UMAP")
+print('plotting UMAP')
 get_Umap(matrix,save_loc=out_path)
 
-print("KNN impute")
+print('KNN impute')
 df_impute = apply_KNN_impute(big_df,2)
-df_impute.to_csv(path+"/imputed.csv")
+df_impute.to_csv(path+'/imputed.csv')
 # get the UMAP
 
-print("plotting sim matrix, impute")
-plot_sim_matrix(df_impute.to_numpy(),indices,chromosomes,"impute",save_loc=out_path)
-print("plotting UMAP, impute")
-get_Umap(df_impute.to_numpy(),"impute",save_loc=out_path)
+print('plotting sim matrix, impute')
+plot_sim_matrix(df_impute.to_numpy(),indices,chromosomes,'impute',save_loc=out_path)
+print('plotting UMAP, impute')
+get_Umap(df_impute.to_numpy(),'impute',save_loc=out_path)
 
-print("Done")
+print('Done')
 
 
 
