755db2c4891cb517dae75f4231b8f2de9443996e
Alex <64327268+AlexDeLos@users.noreply.github.com>
2025-04-09 12:49:32 +0200

    clusters

--- helpers.py
+++ helpers.py
@@ -156,9 +156,9 @@ def apply_KNN_impute(df:pd.DataFrame,n_neighbors: int):
 
 
 def hierarchical_clustering(data_matrix:np.array, path:str, name:str):
-    linkage_data = linkage(data_matrix, method='ward', metric='euclidean')
+    linkage_data = linkage(data_matrix, method='ward', metric='euclidean', optimal_ordering=True)
     dendrogram(linkage_data, no_labels= True)
-    plt.savefig(path+'/cluster_'+name+'.svg')
+    plt.savefig(path+'/cluster_ordered_'+name+'.svg')
     plt.close()
 
 
