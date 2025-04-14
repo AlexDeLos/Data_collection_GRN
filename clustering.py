53d56ec61afdb077f88ab86ebfb3885d44a1dd05
Alex <64327268+AlexDeLos@users.noreply.github.com>
2025-04-14 14:44:21 +0200

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -30,42 +30,67 @@ def hierarchical_clustering_with_colinearity(df, threshold=0.9):
     clusters = fcluster(Z, 1 - threshold, criterion='distance')
     
     return clusters
+def get_study(sample: str):
+    return int(sample.split('_')[-1])
+
 
 path = '/tudelft.net/staff-umbrella/AT GE Datasets/df_local/df/'
-# path = 'df/'
-names = ['corrected','robust','standardized']
+path = 'df/'
 out_path = '/tudelft.net/staff-umbrella/AT GE Datasets/clustered_figures'
-# out_path = 'clustered_figures'
+out_path = 'clustered_figures'
+
+
+names = ['corrected','robust','standardized']
+chromosomes = ['1','2','3','4','5']
 th_list = [0.9,0.8,0.75]
 for name in names:
     print('Starting:', name)
     for th in th_list:
+        print('Starting:', th)
         try:
-            print('reading')
             # raise FileNotFoundError
             cluster_means = pd.read_csv(path+'averaged_clustered_'+str(th)+'_'+name+'.csv', index_col=0)
+            data = pd.read_csv(path+'clustered_'+str(th)+'_'+name+'.csv', index_col=0)
         except FileNotFoundError as e:
             data = pd.read_csv(path+name+'.csv', index_col=0)
             clusters = hierarchical_clustering_with_colinearity(data.T,threshold = th)
             data['cluster'] = clusters
-            # data.to_csv(path+'clustered_'+name)
-            # df_clust = pd.read_csv(path+'clustered_'+name+'_'+str(th), index_col=0)
+            data.to_csv(path+'clustered_'+str(th)+'_'+name+'.csv')
             # Group by cluster and take the mean of each cluster
             cluster_means = data.groupby('cluster').mean()
             # Save the averaged clusters
             cluster_means.to_csv(path+'averaged_clustered_'+str(th)+'_'+name+'.csv')
-        
+        df_clust = data
         print('For theshold ',th, 'we found ', len(cluster_means),' clusters')
+        chunks = len(chromosomes)
+        indx = []
+        chunck_size = len(cluster_means)//chunks
+        for chunk in range(chunks):
+            indx.append(chunk*chunck_size)
+        # indx.append(len(cluster_means))
 
 
-        def get_study(sample: str):
-            return int(sample.split('_')[-1])
+        chunks_ = len(chromosomes)
+        indx_ = []
+        chunck_size_ = len(df_clust)//chunks_
+        for chunk_ in range(chunks_):
+            indx_.append(chunk_*chunck_size_)
+        # indx_.append(len(df_clust))
+
+
+        cluster_means.sort_index(inplace=True)
+        df_clust.sort_values('cluster', inplace=True)
         study_map = list(map(get_study,cluster_means.columns))
+        
 
         print("Starting plotting")
+        # plot sim matrix 
+        plot_sim_matrix(cluster_means,indx,chromosomes,name='_Gene_'+name+'_'+str(th),save_loc=out_path,title='Gen Sim (Not clustered)')
+        plot_sim_matrix(df_clust,indx_,chromosomes,name='_Cluster_'+name+'_'+str(th),save_loc=out_path,title='Cluster Sim (Clustered)')
+
         # plot_sim_matrix(cluster_means,name='clustered', save_loc=out_path, title= 'Data full sim mat (clustered)')
-        get_Umap(cluster_means.to_numpy(),name='_genes_'+name+'_'+str(th),save_loc=out_path, title='Gene expressions (clustered)')
-        get_Umap(cluster_means.to_numpy().T,name='_samples_'+name+'_'+str(th),study_map=study_map,save_loc=out_path, title='Samples coloured by study (clustered)')
-        plot_sim_matrix(cluster_means.to_numpy().T,name='_'+name+'_'+str(th),save_loc=out_path,title='Samples full sim mat (clustered)')
+        # get_Umap(cluster_means.to_numpy(),name='_genes_'+name+'_'+str(th),save_loc=out_path, title='Gene expressions (clustered)')
+        # get_Umap(cluster_means.to_numpy().T,name='_samples_'+name+'_'+str(th),study_map=study_map,save_loc=out_path, title='Samples coloured by study (clustered)')
+        # plot_sim_matrix(cluster_means.to_numpy().T,name='_'+name+'_'+str(th),save_loc=out_path,title='Samples full sim mat (clustered)')
         print("Done plotting")
 
